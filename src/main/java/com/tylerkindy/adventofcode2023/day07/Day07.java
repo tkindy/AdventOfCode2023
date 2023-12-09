022bf34fcbd907779fa2d3131bdffd7dee6c062d
Tyler Kindy <tylerkindy@gmail.com>
2023-12-09 16:35:14 -0500

    Set up day 7

---
+++ src/main/java/com/tylerkindy/adventofcode2023/day07/Day07.java
@@ -0,0 +1,10 @@
+package com.tylerkindy.adventofcode2023.day07;
+
+import com.tylerkindy.adventofcode2023.Utils;
+
+public class Day07 {
+
+  public static void main(String[] args) {
+    String input = Utils.readInput(7);
+  }
+}
---
+++ src/test/java/com/tylerkindy/adventofcode2023/day07/Day07Test.java
@@ -0,0 +1,3 @@
+package com.tylerkindy.adventofcode2023.day07;
+
+class Day07Test {}
