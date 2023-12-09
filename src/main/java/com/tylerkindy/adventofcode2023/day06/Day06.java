0f66e802b28b9c0062f05b9603c7103f44e2c2b6
Tyler Kindy <tylerkindy@gmail.com>
2023-12-09 09:37:47 -0500

    Parse day 6 input

---
+++ src/main/java/com/tylerkindy/adventofcode2023/day06/Day06.java
@@ -0,0 +1,73 @@
+package com.tylerkindy.adventofcode2023.day06;
+
+import com.google.common.collect.ImmutableList;
+import com.tylerkindy.adventofcode2023.Utils;
+import java.time.Duration;
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Day06 {
+
+  private static final Pattern NUMBER = Pattern.compile("\\d+");
+
+  public static void main(String[] args) {
+    List<Race> races = parseRaces(Utils.readInput(6));
+  }
+
+  public static List<Race> parseRaces(String input) {
+    String[] lines = input.trim().split("\n");
+    List<Long> times = parseLine(lines[0]);
+    List<Long> distances = parseLine(lines[1]);
+
+    ImmutableList.Builder<Race> races = ImmutableList.builder();
+
+    for (int i = 0; i < times.size(); i++) {
+      races.add(
+        new Race(
+          Duration.ofMillis(times.get(i)),
+          Distance.ofMillimeters(distances.get(i))
+        )
+      );
+    }
+
+    return races.build();
+  }
+
+  private static List<Long> parseLine(String line) {
+    Matcher matcher = NUMBER.matcher(line);
+
+    ImmutableList.Builder<Long> numbers = ImmutableList.builder();
+    while (matcher.find()) {
+      numbers.add(Long.parseLong(matcher.group()));
+    }
+
+    return numbers.build();
+  }
+
+  public record Race(Duration duration, Distance recordDistance) {}
+
+  public static final class Distance {
+
+    private final long millimeters;
+
+    private Distance(long millimeters) {
+      this.millimeters = millimeters;
+    }
+
+    public static Distance ofMillimeters(long millimeters) {
+      return new Distance(millimeters);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      return o instanceof Distance d && millimeters == d.millimeters;
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(millimeters);
+    }
+  }
+}
---
+++ src/test/java/com/tylerkindy/adventofcode2023/day06/Day06Test.java
@@ -0,0 +1,30 @@
+package com.tylerkindy.adventofcode2023.day06;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.tylerkindy.adventofcode2023.day06.Day06.Distance;
+import com.tylerkindy.adventofcode2023.day06.Day06.Race;
+import java.time.Duration;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class Day06Test {
+
+  @Test
+  void itParsesRaces() {
+    assertThat(
+      Day06.parseRaces(
+        """
+        Time:      7  15   30
+        Distance:  9  40  200"""
+      )
+    )
+      .isEqualTo(
+        List.of(
+          new Race(Duration.ofMillis(7), Distance.ofMillimeters(9)),
+          new Race(Duration.ofMillis(15), Distance.ofMillimeters(40)),
+          new Race(Duration.ofMillis(30), Distance.ofMillimeters(200))
+        )
+      );
+  }
+}
