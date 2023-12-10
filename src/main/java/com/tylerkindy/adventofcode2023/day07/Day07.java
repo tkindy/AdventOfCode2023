433624a3fe99e2c333a760db1d11bd14fad7e63c
Tyler Kindy <tylerkindy@gmail.com>
2023-12-09 19:20:13 -0500

    Parse hands and bids

--- src/main/java/com/tylerkindy/adventofcode2023/day07/Day07.java
+++ src/main/java/com/tylerkindy/adventofcode2023/day07/Day07.java
@@ -1,10 +1,87 @@
 package com.tylerkindy.adventofcode2023.day07;
 
+import com.google.common.collect.ImmutableMultiset;
+import com.google.common.collect.Multiset;
 import com.tylerkindy.adventofcode2023.Utils;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Day07 {
 
+  private static final Pattern HAND_AND_BID = Pattern.compile(
+    "(?<hand>\\w+) (?<bid>\\d+)"
+  );
+
   public static void main(String[] args) {
-    String input = Utils.readInput(7);
+    Multiset<HandAndBid> handsAndBids = parseHandsAndBids(Utils.readInput(7));
+  }
+
+  public static Multiset<HandAndBid> parseHandsAndBids(String input) {
+    return input
+      .lines()
+      .map(Day07::parseHandAndBid)
+      .collect(ImmutableMultiset.toImmutableMultiset());
+  }
+
+  private static HandAndBid parseHandAndBid(String line) {
+    Matcher matcher = HAND_AND_BID.matcher(line);
+    if (!matcher.matches()) {
+      throw new IllegalArgumentException("Unexpected hand and bid line: " + line);
+    }
+
+    Hand hand = parseHand(matcher.group("hand"));
+    int bid = Integer.parseInt(matcher.group("bid"));
+
+    return new HandAndBid(hand, bid);
+  }
+
+  private static Hand parseHand(String handStr) {
+    return new Hand(handStr.chars().mapToObj(Card::fromLabel).toList());
+  }
+
+  public record HandAndBid(Hand hand, int bid) {}
+
+  public record Hand(List<Card> cards) {}
+
+  public enum Card {
+    ACE(12),
+    KING(11),
+    QUEEN(10),
+    JACK(9),
+    TEN(8),
+    NINE(7),
+    EIGHT(6),
+    SEVEN(5),
+    SIX(4),
+    FIVE(3),
+    FOUR(2),
+    THREE(1),
+    TWO(0);
+
+    private final int rank;
+
+    Card(int rank) {
+      this.rank = rank;
+    }
+
+    public static Card fromLabel(int label) {
+      return switch (label) {
+        case 'A' -> ACE;
+        case 'K' -> KING;
+        case 'Q' -> QUEEN;
+        case 'J' -> JACK;
+        case 'T' -> TEN;
+        case '9' -> NINE;
+        case '8' -> EIGHT;
+        case '7' -> SEVEN;
+        case '6' -> SIX;
+        case '5' -> FIVE;
+        case '4' -> FOUR;
+        case '3' -> THREE;
+        case '2' -> TWO;
+        default -> throw new IllegalArgumentException("Unexpected card label: " + label);
+      };
+    }
   }
 }
--- src/test/java/com/tylerkindy/adventofcode2023/day07/Day07Test.java
+++ src/test/java/com/tylerkindy/adventofcode2023/day07/Day07Test.java
@@ -1,3 +1,42 @@
 package com.tylerkindy.adventofcode2023.day07;
 
-class Day07Test {}
+import static com.tylerkindy.adventofcode2023.day07.Day07.Card.ACE;
+import static com.tylerkindy.adventofcode2023.day07.Day07.Card.FIVE;
+import static com.tylerkindy.adventofcode2023.day07.Day07.Card.JACK;
+import static com.tylerkindy.adventofcode2023.day07.Day07.Card.KING;
+import static com.tylerkindy.adventofcode2023.day07.Day07.Card.QUEEN;
+import static com.tylerkindy.adventofcode2023.day07.Day07.Card.SEVEN;
+import static com.tylerkindy.adventofcode2023.day07.Day07.Card.SIX;
+import static com.tylerkindy.adventofcode2023.day07.Day07.Card.TEN;
+import static com.tylerkindy.adventofcode2023.day07.Day07.Card.THREE;
+import static com.tylerkindy.adventofcode2023.day07.Day07.Card.TWO;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.tylerkindy.adventofcode2023.day07.Day07.Hand;
+import com.tylerkindy.adventofcode2023.day07.Day07.HandAndBid;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class Day07Test {
+
+  @Test
+  void itParses() {
+    assertThat(
+      Day07.parseHandsAndBids(
+        """
+        32T3K 765
+        T55J5 684
+        KK677 28
+        KTJJT 220
+        QQQJA 483"""
+      )
+    )
+      .containsExactlyInAnyOrder(
+        new HandAndBid(new Hand(List.of(THREE, TWO, TEN, THREE, KING)), 765),
+        new HandAndBid(new Hand(List.of(TEN, FIVE, FIVE, JACK, FIVE)), 684),
+        new HandAndBid(new Hand(List.of(KING, KING, SIX, SEVEN, SEVEN)), 28),
+        new HandAndBid(new Hand(List.of(KING, TEN, JACK, JACK, TEN)), 220),
+        new HandAndBid(new Hand(List.of(QUEEN, QUEEN, QUEEN, JACK, ACE)), 483)
+      );
+  }
+}
